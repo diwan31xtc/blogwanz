0975f06726c3679e741ab2c95f1fb085c0edc76c
diwan31xtc <43528703+diwan31xtc@users.noreply.github.com>
2019-01-28 21:48:06 +0700

    cs safelink

---
+++ safelink.js
@@ -0,0 +1,76 @@
+<div dir="ltr" style="text-align: left;" trbidi="on">
+<div dir="ltr" style="text-align: left;" trbidi="on">
+<style>
+#outer-wrapper{margin:0 auto;text-align:left;float:none;background-position:center!important}
+#post-wrapper{width:100%;max-width:100%;margin:0 auto;text-align:left;float:none;background-position:center!important}
+.post-body,.post{background-position:center!important}
+h2.post-title a,h1.post-title a,h2.post-title,h1.post-title{display:none;margin-top:0;margin:0}
+#blog1,#artikel,.blog-posts{background-position:center!important}
+.breadcrumbs{display:none;margin-top:0;margin:0}
+#comments,#breadcrumb,#sidebar-wrapper,#menu-wrap{display:none;margin-top:0;margin:0}
+.post-inner{padding:0 0 0 0;margin:20px auto}
+.post-body ul#wrapin{display:block;position:relative;margin:30px auto 0 auto}
+.post-body ul#wrapin li{display:block;margin:0 auto;text-align:left}
+.post-body ul#wrapin br{display:none}
+button{color:#fff!important;height:22px;font-size:12px;font-weight:bold;background:#2d8659;padding:3px 10px;border-radius:4px;border:none;outline:none;box-shadow:0 2px 5px 0 rgba(0,0,0,0.16),0 2px 10px 0 rgba(0,0,0,0.12);cursor:pointer;transition:all .4s ease-in-out;text-transform:uppercase;}
+</style>
+<div class="separator" style="clear: both; text-align: center;">
+KODE IKLAN</div>
+<div>
+LIFE insurance is among the oldest financial products. The Amicable Society, founded in London in 1706, charged members a set contribution and paid out annually to widows and children of those who had died in the previous 12 months. Today it is a vast industry: life and health insurers employ over 800,000 people in America alone. It protects hundreds of millions against the risk of dying early, through death benefits, or the risk of living longer than expected, for example through annuities. According to Allianz, a German insurer, total life-insurance premiums are above 5% of GDP in many rich countries, including Britain, France, Italy and Japan. In America, the world’s biggest market, annual premiums total more than $550bn.<br />
+<br />
+But life insurers are struggling as never before. Those parts of the industry that have not evolved fast enough, says Clive Bannister, the head of Phoenix Group, a “closed” life insurer that buys and manages old policies but issues no new ones, have experienced a “demolition”. He lists half a dozen British life insurers that existed 15 years ago but no longer do.<br />
+<br />
+The lacklustre partial spin-off on May 10th of the American life-insurance unit of AXA, a French insurer, is just the latest sign of worries about the industry’s prospects. Shares were priced at $20, well below the expected $24-27. Among its woes are low interest rates, which make it hard to fulfil promises of guaranteed returns on some products, and costly new regulations. Since 2016 Europe has required much more capital to be held against long-term liabilities, like those of life insurers. That has prompted some to seek to rebalance their businesses. AXA has not only listed its American life arm this year but also announced the purchase of XL, a Bermuda-based property-and-casualty insurer.<br />
+<br />
+More serious still are demographic pressures. As the rich world ages and retires, total life-insurance premiums are flat or falling. In developed countries they fell by 0.5% in 2016 in real terms, according to Swiss Re, a reinsurer. Some countries have fallen off a cliff, including Australia (an 18.2% drop in 2017 in nominal terms) and Japan (11.3%), where negative interest rates have savaged returns and prompted some life insurers to stop selling lump-sum death-benefit policies. The industry has long been used to accumulating new assets, with old policies sold off to specialists (such as Phoenix). It will now have to adjust to “decumulation”, says Henrik Naujoks of Bain &amp; Company, a consultancy.<br />
+<br />
+<center>
+<script src="LINK-JAVASCRIPT" type="text/javascript"></script>
+<div class="fitur-body">
+<div class="separator" style="clear: both; text-align: center;">
+KODE IKLAN
+</div>
+<script type="text/javascript">
+//<![CDATA[
+function changeLink() {
+    var e = Base64.decode(res);
+    window.open(e, "_blank")
+}
+var currentURL = location.href,
+    str = currentURL,
+    res = str.replace("https://blog-artikelnews.blogspot.com/p/go-to-url.html?url=", "");
+document.write('<button target="_self" class="btn btn-success" style="display: block;" id="download" onclick="changeLink()">Get Link >></button>');
+onload: generate();
+//]]>
+</script>
+</div>
+</center>
+<div class="separator" style="clear: both; text-align: center;">
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<ins class="adsbygoogle"
+     style="display:block; text-align:center;"
+     data-ad-layout="in-article"
+     data-ad-format="fluid"
+     data-ad-client="ca-pub-4368157617739122"
+     data-ad-slot="9989118001"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+</div>
+</div>
+<br />
+One way out of this bind is to add bells and whistles to their basic offerings. A survey by Bain suggested that customers liked the idea of receiving advice during illness or regular health check-ups as part of their life-insurance policy. That suggests they might welcome a blurring of the distinction between life and health insurance.<br />
+<br />
+Another option is to expand into new markets. In emerging economies, life-insurance penetration ranges from 2.6% of GDP in China to just 0.4% in Russia. (South Africa, at 11%, is an outlier.) Total premiums grew by 16.9% in real terms in 2016. But competition from domestic incumbents is fierce, particularly in China. And attracting new customers or providing new services, whether at home or abroad, will be hard for an industry that is saddled with high costs and has been slow to go digital. Most sales are still through agents and brokers.<br />
+<br />
+A third approach is to seek new kinds of customers. Though death benefits and annuities are still the core business, life insurers are branching out into savings products and insurance against disability or needing long-term social care. Greg Galeaz of PwC, a consultancy, notes that American firms are crafting insurance products to appeal to younger people, such as policies that allow withdrawals to pay off student loans or support ageing parents. The industry could do more to appeal to the moderately affluent, who largely think of life insurers as providers of death benefits rather than savings and retirement products.<br />
+<br />
+<div class="separator" style="clear: both; text-align: center;">
+KODE IKLAN
+</div>
+<br />
+Insurers’ single most popular strategy has been to diversify into investment-like products—an index-tracker bundled with a guarantee that the principal will be returned, for example. But since these resemble the offerings of asset managers, they are also the least distinctive. Antimo Perretta of AXA echoes many of his peers when he says that risk-averse customers have much to gain from the smoothing of returns an insurer can provide. Life insurers also argue they give access to a broader range of asset classes (including, say, exposure to private equity) than asset managers do. And they point to the attractive tax treatment in most jurisdictions of savings products with a life-insurance element.<br />
+<br />
+But that is to reverse the logic. Governments tax life insurance lightly because of the social value of protecting people from underestimating their life expectancy and retirement needs. Australia used to offer retiring workers incentives to purchase annuities; when it stopped these in 2007, the annuities market shrivelled away. Many elderly Australians have used their freedom imprudently: around half have emptied their pension pot by the age of 70. Mr Bannister fears a repeat on a grander scale in Britain, which in 2015 abolished a requirement for retirees to purchase an annuity. Life insurers must somehow reinvent themselves without losing sight of their core purpose: providing a way for their customers to plan for a dignified old age without overburdening the state.</div>
+</div>
